434bd6817795fc3af3f88d180c2e1cac1fb106ac
zyxazhang <17379744014@163.com>
2021-08-31 21:19:36 +0800

    monir:取消修改内容

--- frontend/pc/src/views/project/index.vue
+++ frontend/pc/src/views/project/index.vue
@@ -57,15 +57,6 @@
             this.getProjectList()
             if (this.$store.state.project.id) {
                 this.getProjectDetail()
-            } else {
-                const projectsWithViewPerm = this.$store.state.projectList.filter(item => item.auth_actions.includes('project_view'))
-                if (projectsWithViewPerm.length === 0) {
-                    this.$router.replace({ name: 'ProjectGuide' })
-                } else {
-                    this.$store.commit('project/setProjectId', projectsWithViewPerm[0].key)
-                    this.$store.dispatch('project/changeDefaultProject', projectsWithViewPerm[0].key)
-                    this.$router.replace({ name: 'projectTicket', query: { project_id: projectsWithViewPerm[0].key } })
-                }
             }
         },
         methods: {
@@ -84,6 +75,16 @@
                     this.$store.commit('project/setProjectListLoading', true)
                     const res = await this.$store.dispatch('project/getProjectList')
                     this.$store.commit('project/setProjectList', res.data.items)
+                    if (!this.$store.state.project.id) {
+                        const projectsWithViewPerm = res.data.items.filter(item => item.auth_actions.includes('project_view'))
+                        if (projectsWithViewPerm.length === 0) {
+                            this.$router.replace({ name: 'ProjectGuide' })
+                        } else {
+                            this.$store.commit('project/setProjectId', projectsWithViewPerm[0].key)
+                            this.$store.dispatch('project/changeDefaultProject', projectsWithViewPerm[0].key)
+                            this.$router.replace({ name: 'projectTicket', query: { project_id: projectsWithViewPerm[0].key } })
+                        }
+                    }
                 } catch (e) {
                     errorHandler(e, this)
                 } finally {
