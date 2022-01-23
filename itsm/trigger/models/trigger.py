516e94d33bbaa5f36a190be5b4219ae00093a236
hanshuaikang <1758504262@qq.com>
2022-01-23 16:42:35 +0800

    bugfix: 触发器克隆添加版本信息

--- itsm/trigger/models/trigger.py
+++ itsm/trigger/models/trigger.py
@@ -22,7 +22,7 @@ NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
 WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
-
+import datetime
 from itertools import chain
 from django.db import models
 from django.utils.translation import ugettext as _
@@ -41,7 +41,8 @@ from itsm.component.constants import (
     OPT_TYPE_CHOICE,
     TRIGGER_SOURCE_TYPE,
     TRIGGER_ICON_CHOICE,
-    TRIGGER_SIGNAL, LEN_SHORT,
+    TRIGGER_SIGNAL,
+    LEN_SHORT,
 )
 from .base import Model as TriggerBaseModel
 from ...project.models import Project
@@ -68,9 +69,12 @@ class ActionSchema(TriggerBaseModel):
     display_name = models.CharField(_("动作显示名称"), max_length=LEN_LONG, null=True)
     component_type = models.CharField(_("组件类型"), max_length=LEN_LONG)
 
-    operate_type = models.CharField(_("操作方式"), choices=OPT_TYPE_CHOICE, max_length=LEN_NORMAL,
-                                    default="BACKEND")
-    delay_params = jsonfield.JSONField(_("延迟参数"), default={"type": "custom", "value": 0})
+    operate_type = models.CharField(
+        _("操作方式"), choices=OPT_TYPE_CHOICE, max_length=LEN_NORMAL, default="BACKEND"
+    )
+    delay_params = jsonfield.JSONField(
+        _("延迟参数"), default={"type": "custom", "value": 0}
+    )
     can_repeat = models.BooleanField(_("是否可以重复执行"), default=False)
 
     params = jsonfield.JSONField(_("配置参数"), help_text=_("当前响应事件配置的参数模版"), default={})
@@ -86,7 +90,9 @@ class ActionSchema(TriggerBaseModel):
 
     @property
     def component_class(self):
-        return ComponentLibrary.get_component_class("trigger", component_code=self.component_type)
+        return ComponentLibrary.get_component_class(
+            "trigger", component_code=self.component_type
+        )
 
     def clone(self):
         self.id = None
@@ -97,12 +103,12 @@ class ActionSchema(TriggerBaseModel):
     def tag_data(self):
         data = {}
         for f in chain(self._meta.concrete_fields, self._meta.private_fields):
-            if not getattr(f, 'editable', False):
+            if not getattr(f, "editable", False):
                 continue
             if isinstance(f, models.ForeignKey):
-                data['{}_id'.format(f.name)] = getattr(getattr(self, f.name), 'id', '')
+                data["{}_id".format(f.name)] = getattr(getattr(self, f.name), "id", "")
             else:
-                data[f.name] = getattr(self, f.name, '')
+                data[f.name] = getattr(self, f.name, "")
         return data
 
 
@@ -121,21 +127,28 @@ class Trigger(TriggerBaseModel):
         "is_draft",
         "is_enabled",
         "icon",
-        "project_key"
+        "project_key",
     )
 
     name = models.CharField(_("名称"), max_length=LEN_NORMAL)
     desc = models.CharField(_("描述信息"), max_length=LEN_X_LONG)
 
     signal = models.CharField(_("触发事件信号"), null=False, max_length=LEN_MIDDLE)
-    sender = models.CharField(_("触发对象"), help_text=_("一般为触发该信号的实际对象模型id"), max_length=LEN_NORMAL)
+    sender = models.CharField(
+        _("触发对象"), help_text=_("一般为触发该信号的实际对象模型id"), max_length=LEN_NORMAL
+    )
 
     # inputs 好像暂时没需要
-    inputs = jsonfield.JSONField(_("输入参数"), help_text=_("输入参数引用的参数变量"), default=EMPTY_LIST)
+    inputs = jsonfield.JSONField(
+        _("输入参数"), help_text=_("输入参数引用的参数变量"), default=EMPTY_LIST
+    )
 
     # 触发器的来源, 可以根据用户的需要来定义
     source_type = models.CharField(
-        _("来源类型"), help_text=_("触发规则的配置来源"), choices=TRIGGER_SOURCE_TYPE, max_length=LEN_NORMAL,
+        _("来源类型"),
+        help_text=_("触发规则的配置来源"),
+        choices=TRIGGER_SOURCE_TYPE,
+        max_length=LEN_NORMAL,
     )
     source_id = models.IntegerField(_("对应的来源PK值"), null=True, blank=True)
 
@@ -143,9 +156,12 @@ class Trigger(TriggerBaseModel):
 
     is_draft = models.BooleanField(_("是否为草稿"), default=True)
     is_enabled = models.BooleanField(_("是否可启用"), default=False)
-    icon = models.CharField(_("对应的icon"), default=EMPTY_STRING, choices=TRIGGER_ICON_CHOICE,
-                            max_length=64)
-    project_key = models.CharField(_("项目key"), max_length=LEN_SHORT, null=False, default=0)
+    icon = models.CharField(
+        _("对应的icon"), default=EMPTY_STRING, choices=TRIGGER_ICON_CHOICE, max_length=64
+    )
+    project_key = models.CharField(
+        _("项目key"), max_length=LEN_SHORT, null=False, default=0
+    )
 
     need_auth_grant = True
 
@@ -165,13 +181,23 @@ class Trigger(TriggerBaseModel):
             {
                 "conditions": item.condition
                 if item.by_condition
-                else {"all": [
-                    {"name": "constant_bool_true", "operator": "is_true", "value": True, }]},
+                else {
+                    "all": [
+                        {
+                            "name": "constant_bool_true",
+                            "operator": "is_true",
+                            "value": True,
+                        }
+                    ]
+                },
                 "actions": [
                     {
                         "name": "trigger_handle",
-                        "params": {"rule": item, "source_type": source_type,
-                                   "source_id": source_id},
+                        "params": {
+                            "rule": item,
+                            "source_type": source_type,
+                            "source_id": source_id,
+                        },
                     }
                 ],
             }
@@ -191,6 +217,8 @@ class Trigger(TriggerBaseModel):
         克隆功能
         """
         self.id = None
+        version_number = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        self.name = "{}{}".format(self.name, version_number)
         self.source_id = dst_source_id
         self.source_type = dst_source_type
         if dst_sender:
@@ -211,12 +239,12 @@ class Trigger(TriggerBaseModel):
             rules.append(rule.tag_data())
 
         for f in chain(self._meta.concrete_fields, self._meta.private_fields):
-            if not getattr(f, 'editable', False):
+            if not getattr(f, "editable", False):
                 continue
             if isinstance(f, models.ForeignKey):
-                data['{}_id'.format(f.name)] = getattr(getattr(self, f.name), 'id', '')
+                data["{}_id".format(f.name)] = getattr(getattr(self, f.name), "id", "")
             else:
-                data[f.name] = getattr(self, f.name, '')
+                data[f.name] = getattr(self, f.name, "")
         return data
 
     @property
@@ -224,7 +252,7 @@ class Trigger(TriggerBaseModel):
         for _type, signals in TRIGGER_SIGNAL.items():
             if self.signal in signals:
                 return _type
-        return 'Undefined'
+        return "Undefined"
 
 
 class TriggerRule(TriggerBaseModel):
@@ -238,8 +266,10 @@ class TriggerRule(TriggerBaseModel):
     condition = jsonfield.JSONField(_("触发条件"), default=EMPTY_DICT)
     by_condition = models.BooleanField(_("触发方式"), default=False)
     action_schemas = jsonfield.JSONCharField(
-        _("响应事件列表"), help_text=_("关联事件的配置ID列表,关联actionSchema"), max_length=LEN_LONG,
-        default=EMPTY_LIST
+        _("响应事件列表"),
+        help_text=_("关联事件的配置ID列表,关联actionSchema"),
+        max_length=LEN_LONG,
+        default=EMPTY_LIST,
     )
     trigger_id = models.IntegerField(_("对应的触发器id"), null=True)
 
@@ -260,6 +290,8 @@ class TriggerRule(TriggerBaseModel):
         for action_schema in ActionSchema.objects.filter(id__in=self.action_schemas):
             data_action_schemas.append(action_schema.tag_data())
         return dict(
-            name=self.name, condition=self.condition, by_condition=self.by_condition,
-            action_schemas=data_action_schemas
+            name=self.name,
+            condition=self.condition,
+            by_condition=self.by_condition,
+            action_schemas=data_action_schemas,
         )
