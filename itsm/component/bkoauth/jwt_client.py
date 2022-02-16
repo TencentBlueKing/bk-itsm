b3b470cedcc2d75ac05eab3a149a394aaac15a00
hanshuaikang <1758504262@qq.com>
2022-02-16 21:33:08 +0800

    feature: 添加部分日志打印, 修改headers key

--- itsm/component/bkoauth/jwt_client.py
+++ itsm/component/bkoauth/jwt_client.py
@@ -31,7 +31,7 @@ LOG = logging.getLogger("component")
 class JWTClient(object):
     JWT_KEY_NAME = "HTTP_X_BKAPI_JWT"
     JWT_PUBLIC_KEY_HEADER_NAME = "HTTP_X_BKAPI_PUBLIC_KEY"
-    JWT_RESOURCE = "HTTP-X-BKAPI-SOURCE"
+    JWT_RESOURCE = "X-BKAPI-SOURCE"
 
     def __init__(self, request):
         self.request = request
@@ -100,7 +100,9 @@ class JWTClient(object):
         首先从请求 Header X-Bkapi-Public-Key 获取 JWT Public-Key，
         若 Header 中不存在，或者解析失败，则使用默认值
         """
+        logger.info("requests.META == {}".format(self.request.META))
         jwt_resource = self.request.META.get(self.JWT_RESOURCE, "ESB")
+        logger("the api resource is {}".format(jwt_resource))
         if jwt_resource == "APIGW":
             return settings.APIGW_PUBLIC_KEY
         else:
