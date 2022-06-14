561743feec672edfebaeae2a7d7c6ef283eb3a19
hanshuaikang <1758504262@qq.com>
2022-06-14 20:01:08 +0800

    feature: 新增网关同步命令

--- bin/start_web.sh
+++ bin/start_web.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-python manage.py fetch_apigw_public_key  
-python manage.py fetch_esb_public_key
+python manage.py sync_saas_apigw
 python manage.py collectstatic --noinput
 gunicorn wsgi -w 1 -b :$PORT --access-logfile - --error-logfile - --access-logformat '[%(h)s] %({request_id}i)s %(u)s %(t)s "%(r)s" %(s)s %(D)s %(b)s "%(f)s" "%(a)s"' --max-requests=200 --log-level debug --timeout 120
