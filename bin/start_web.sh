946dd23d9a8bc34cfe95c5185a4fb2e5c82d7c7b
hanshuaikang <1758504262@qq.com>
2022-08-31 20:19:33 +0800

    minor: 容器化版本默认使用多线程启动

--- bin/start_web.sh
+++ bin/start_web.sh
@@ -2,4 +2,4 @@
 
 python manage.py sync_saas_apigw
 python manage.py collectstatic --noinput
-gunicorn wsgi -w 1 -b :$PORT --access-logfile - --error-logfile - --access-logformat '[%(h)s] %({request_id}i)s %(u)s %(t)s "%(r)s" %(s)s %(D)s %(b)s "%(f)s" "%(a)s"' --max-requests=200 --log-level debug --timeout 120
+gunicorn wsgi -w 1 -b :$PORT -k gthread --threads 3 --timeout 600 --max-requests 500 --max-requests-jitter 100 --access-logfile - --error-logfile - --access-logformat '[%(h)s] %({request_id}i)s %(u)s %(t)s "%(r)s" %(s)s %(D)s %(b)s "%(f)s" "%(a)s"'
