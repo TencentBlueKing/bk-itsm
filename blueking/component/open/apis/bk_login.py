aa5cc3dec3b60cce39291f6fb21caa0ddc57904b
hanshuaikang <1758504262@qq.com>
2023-07-19 20:03:50 +0800

    minor: 升级blueapps版本

--- blueking/component/open/apis/bk_login.py
+++ blueking/component/open/apis/bk_login.py
@@ -33,27 +33,39 @@ class CollectionsBkLogin(object):
         self.client = client
 
         self.get_all_users = ComponentAPI(
-            client=self.client, method='GET',
-            path='/api/c/compapi{bk_api_ver}/bk_login/get_all_users/',
-            description=u'获取所有用户信息'
+            client=self.client,
+            method="GET",
+            path="/api/c/compapi{bk_api_ver}/bk_login/get_all_users/",
+            description="获取所有用户信息",
         )
         self.get_batch_users = ComponentAPI(
-            client=self.client, method='POST',
-            path='/api/c/compapi{bk_api_ver}/bk_login/get_batch_users/',
-            description=u'批量获取用户信息'
+            client=self.client,
+            method="POST",
+            path="/api/c/compapi{bk_api_ver}/bk_login/get_batch_users/",
+            description="批量获取用户信息",
         )
         self.get_user = ComponentAPI(
-            client=self.client, method='GET',
-            path='/api/c/compapi{bk_api_ver}/bk_login/get_user/',
-            description=u'获取用户信息'
+            client=self.client,
+            method="GET",
+            path="/api/c/compapi{bk_api_ver}/bk_login/get_user/",
+            description="获取用户信息",
         )
         self.get_all_user = ComponentAPI(
-            client=self.client, method='GET',
-            path='/api/c/compapi{bk_api_ver}/bk_login/get_all_user/',
-            description=u'获取所有用户信息'
+            client=self.client,
+            method="GET",
+            path="/api/c/compapi{bk_api_ver}/bk_login/get_all_user/",
+            description="获取所有用户信息",
         )
         self.get_batch_user = ComponentAPI(
-            client=self.client, method='GET',
-            path='/api/c/compapi{bk_api_ver}/bk_login/get_batch_user/',
-            description=u'获取多个用户信息'
+            client=self.client,
+            method="GET",
+            path="/api/c/compapi{bk_api_ver}/bk_login/get_batch_user/",
+            description="获取多个用户信息",
+        )
+
+        self.is_login = ComponentAPI(
+            client=self.client,
+            method="GET",
+            path="/api/c/compapi{bk_api_ver}/bk_login/is_login/",
+            description="判断用户是否登录",
         )
