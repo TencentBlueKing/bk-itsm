a794318ccd2793613637f122f90e8a13dc51c041
luofann <fannluowork@gmail.com>
2023-09-14 11:58:35 +0800

    fix: 修复蓝盾审批单据自定表格展示跳转链接的问题

--- frontend/weixin/src/components/renderView/tag/tableText.vue
+++ frontend/weixin/src/components/renderView/tag/tableText.vue
@@ -27,10 +27,12 @@
       { 'has-children': form.children && form.children.length }
     ]"
     @click="onInnerLinkClick">
-    <label>{{ form.label }}</label>
-    <span v-if="!Array.isArray(form.value)">{{ form.value }}</span>
-    <template v-else>
-      <TableText v-for="(item, i) in form.value" :key="i" :form="item" />
+    <label>{{ form.label || '--' }}</label>
+    <template v-if="type !== 'url'">
+      <span v-if="!Array.isArray(form.value)">{{ form.value }}</span>
+      <template v-else>
+        <TableText v-for="(item, i) in form.value" :key="i" :form="item" />
+      </template>
     </template>
   </p>
 </template>
@@ -45,6 +47,10 @@ export default {
     form: {
       type: Object,
       default: () => ({})
+    },
+    type: {
+      type: String,
+      default: ''
     }
   },
   setup(props) {
@@ -73,6 +79,7 @@ export default {
 </script>
 <style lang="postcss">
 .table-text {
+  word-break: break-all;
   > label {
     color: #63656e;
   }
