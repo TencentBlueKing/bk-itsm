b2c4b25c94666f43178f47ea8216a565a47f188a
Weiming-Guo-X <15170283905@163.com>
2025-04-18 10:53:58 +0800

    feat:使用环境变量配置是否启用蓝盾节点 --story=122444555 # Reviewed, transaction id: 39329

--- itsm/sites/views.py
+++ itsm/sites/views.py
@@ -116,15 +116,17 @@ def index(request):
         ).value
     except SystemSettings.DoesNotExist:
         notice_center_switch_value = "off"
-    
+
     # 文档地址转换
     doc_lang = "EN"
     lang = get_language()
     if lang in ["zh-cn", "zh-hans"]:
         doc_lang = "ZH"
-    
+
     version = get_version()
-    doc_url = settings.BK_DOC_URL.format(lang=doc_lang, version=get_major_minor_version(version))
+    doc_url = settings.BK_DOC_URL.format(
+        lang=doc_lang, version=get_major_minor_version(version)
+    )
 
     return render(
         request,
@@ -149,6 +151,7 @@ def index(request):
             "BK_PLATFORM_NAME": settings.BK_PLATFORM_NAME,
             "VERSION": version,
             "BKAPP_CSRF_COOKIE_NAME": settings.CSRF_COOKIE_NAME,
+            "BKAPP_CI_ENABLED": settings.BKAPP_CI_ENABLED,
         },
     )
 
@@ -178,16 +181,16 @@ def get_version():
     """
     # 读取文件内容
     app_desc = os.path.join(settings.PROJECT_ROOT, "VERSION")
-    with open(app_desc, 'r') as file:
+    with open(app_desc, "r") as file:
         content = file.read()
     return content.strip()
 
 
 def get_major_minor_version(version_string):
     # 使用 split() 方法分割字符串
-    parts = version_string.split('.')
+    parts = version_string.split(".")
     # 取前两个部分并用 '.' 连接
-    major_minor = '.'.join(parts[:2])
+    major_minor = ".".join(parts[:2])
     return major_minor
 
 
